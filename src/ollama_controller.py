de1cf0fe6d1540bb78114082a93c68cf54e67132
Antoine Lassagne <antoinelassagne@gmail.com>
2025-11-16 21:20:27 +0100

    WIP: async mode in ollama for iterative TTS generation

--- src/ollama_controller.py
+++ src/ollama_controller.py
@@ -1,10 +1,26 @@
+import threading
+
 from ollama import Client
+from PySide6.QtCore import QObject, Signal
 
 from src.types import ErrorCode
 
+SENTENCES_SPLITTERS = [".", "!", "?"]
+MINIMUM_SENTENCE_LENGTH = 20
 
-class OllamaController:
+
+class OllamaController(QObject):
+    # Signal emitted when a new story chunk is ready in async mode
+    story_chunk = Signal(str)
+
+    # contains the story chunk that is ready to be published, or None
+    story_to_publish = None
+
+    # contains the current story being generated
+    story = ""
+
     def __init__(self, host: str):
+        super().__init__()
         print("Hello OllamaController!")
 
         self.client = Client(host="{}:11434".format(host))
@@ -24,9 +40,50 @@ class OllamaController:
     def text_to_seech(self, text):
         print(f"Converting text to speech: {text}")
 
-    def generate_story(self, prompt):
+    def refine_and_publish_story_if_ready(self):
+        if self.refine_story():
+            print("\nOllama > Story chunk ready: ", self.story_to_publish)
+            self.story_chunk.emit(self.story_to_publish)
+            self.story_to_publish = None
+
+    def refine_story(self):
+        if self.story_to_publish is not None:
+            print(
+                "There is already a story chunk ready to publish: {}".format(
+                    self.story_to_publish
+                )
+            )
+            raise Exception(
+                "There is already a story chunk ready to publish {}".format(
+                    self.story_to_publish
+                )
+            )
+
+        for splitter in SENTENCES_SPLITTERS:
+            if splitter in self.story and len(self.story) >= MINIMUM_SENTENCE_LENGTH:
+                split_index = self.story.rfind(splitter) + 1
+                self.story_to_publish = self.story[:split_index].strip()
+                self.story = self.story[split_index:].strip()
+                return True
+        return False
+
+    def generate_story(self, prompt, async_mode: bool = False):
+        if self.running:
+            print("Story generation already in progress.")
+            return "", ErrorCode.BUSY
+        if async_mode:
+            self.generation_thread = threading.Thread(
+                target=self.generate_story_worker, args=(prompt, True), daemon=True
+            )
+            self.generation_thread.start()
+            return "", ErrorCode.SUCCESS
+        else:
+            return self.generate_story_worker(prompt)
+
+    def generate_story_worker(self, prompt, async_mode: bool = False):
+        self.running = True
         print(f"Getting story for prompt: {prompt}")
-        story = ""
+        self.story = ""
         for chunk in self.client.generate(
             model=self.story_model,
             prompt=self.preprompt + prompt,
@@ -35,6 +92,11 @@ class OllamaController:
             # print(chunk)
             # print(type(chunk))
             print(".", end="", flush=True)
-            story += chunk["response"]
-        print("Ollama > Story generation complete: ", story)
-        return story, ErrorCode.SUCCESS
+            story_chunk = chunk["response"]
+            self.story += story_chunk
+            if async_mode:
+                self.refine_and_publish_story_if_ready()
+
+        print("Ollama > Story generation complete: ", self.story)
+        self.running = False
+        return self.story, ErrorCode.SUCCESS
--- src/types.py
+++ src/types.py
@@ -4,3 +4,4 @@ from enum import Enum
 class ErrorCode(Enum):
     SUCCESS = (0,)
     ERROR = 1
+    BUSY = 2
